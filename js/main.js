db2f8abce28fd8a9e10653d78f58a411ea8bf3a3
mmtzitziras <mariostzitziras4@gmail.com>
2024-04-20 16:06:30 +0300

    Added full fanction to operators and the calculator works as it should be

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 let aNumber;
 let bNumber;
 let operator;
-let displayValue = "";
+let displayValue = "0";
+const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const value = document.querySelector(".show-state");
+value.textContent = displayValue;
+
 
 function operate(num1, num2, operator) {
     switch (operator) {
@@ -34,3 +38,57 @@ function divide(a, b) {
     return a / b;
 }
 
+function clear() {
+    displayValue = "0";
+    value.textContent = displayValue;
+}
+
+function equal() {
+    const numsToOperate = displayValue.split(" ");
+    const num1 = Number(numsToOperate[0]);
+    const num2 = Number(numsToOperate[2]);
+    const operator = numsToOperate[1];
+    const result = operate(num1, num2, operator);
+    displayValue = result;
+    value.textContent = displayValue;
+}
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {button.addEventListener("click", doOperation => {
+    if (nums.includes(button.textContent)) {
+        if (value.textContent === "0") {
+            displayValue = "";
+            displayValue = button.textContent;
+            value.textContent = displayValue;
+        }
+        else{
+            displayValue += button.textContent;
+            value.textContent = displayValue;
+        }
+        
+    }
+    else if (button.textContent === ".") {
+        if (value.textContent.includes(".")) {
+            return;
+        }
+        else{
+            displayValue += button.textContent;
+            value.textContent = displayValue;
+        }
+    }
+    else if (button.textContent === "C") {
+        clear();
+        
+    }
+    else if (button.textContent === "=") {
+        equal();
+        
+    }
+    else{   
+        displayValue += " " + button.textContent + " ";
+        value.textContent = displayValue;
+    }
+
+
+})});
